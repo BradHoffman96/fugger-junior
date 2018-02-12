8ce7a1bb20126b792d3d54ccfb186f000f094780
Davis <davisdavis27@gmail.com>
2018-02-11 19:36:31 -0500

    data for a year
    every hour from 11/11/15 to 12/31/16 returned and put in a csv (time can be changed, and i didnt adjust it to fit perfectly in a year) This scraper should work for any of the history endpoints by changing the link. API also has a social media url so we can adapt this to that.

--- capTest.go
+++ capTest.go
@@ -17,9 +17,10 @@ var arbitrary_json map[string]interface{}
 func main() { //https://min-api.cryptocompare.com/data/pricehistorical
   // documentation: https://www.cryptocompare.com/api#-api-data-histohour-
   // test query https://min-api.cryptocompare.com/data/histohour?fsym=BTC&tsym=USD&limit=60&aggregate=3&e=CCCAGG
+
   var fsym string = "BTC" //set of variables to be inserted into URL
   var tsym string = "USD"
-  var limit string = "1000" //api says limit is 2000 but only pulling 667 results?
+  var limit string = "2000" //api says limit is 2000 but only pulling 667 results?
   var e string = "CCCAGG"
 
   csvName := fmt.Sprintf("%s_to_%s_%s.csv", fsym, tsym, e) //add time when I figure the return out better
@@ -28,11 +29,12 @@ func main() { //https://min-api.cryptocompare.com/data/pricehistorical
     log.Fatal(err)
   }
   writer := csv.NewWriter(file)
+  header := []string{"time", "close", "high", "low", "open", "volumefrom", "volumeto"}
+  writer.Write(header) //writer can seemingly only take strings so the conversion below is a bit of a mess
 
-  for x := 1458820800; x <= 1483246800; x += 7192800 {
+  for x := 1454418000; x <= 1486069200; x+= 7200000 {
     xint := strconv.Itoa(x)
-    fmt.Println(xint)
-    url := fmt.Sprintf("https://min-api.cryptocompare.com/data/histohour?fsym=%s&tsym=%s&limit=%s&aggregate=1", fsym, tsym, limit, e)
+    url := fmt.Sprintf("https://min-api.cryptocompare.com/data/histohour?fsym=%s&tsym=%s&limit=%s&aggregate=1&e=%s&toTs=%s", fsym, tsym, limit, e, xint)
     resp, err := http.Get(url) //&toTs allows you to specify a specific time: https://min-api.cryptocompare.com/
     //fsym is from symbol // tsym is to symbol // limit is limit //e is name of exchange
     if err != nil{
@@ -45,18 +47,12 @@ func main() { //https://min-api.cryptocompare.com/data/pricehistorical
       log.Fatal(err)
     }
 
-
-
     json.Unmarshal([]byte(body), &arbitrary_json)
     //.([]interface{}) is a type assertion
     test := arbitrary_json["Data"].([]interface{}) //incredibly helpful https://stackoverflow.com/questions/31815969/go-cannot-range-over-my-var-type-interface
-    //header := []string{"time", "close", "high", "low", "open", "volumefrom", "volumeto"}
-    //writer.Write(header) //writer can seemingly only take strings so the conversion below is a bit of a mess
+
     for i, v := range(test) {
       item := (v.(map[string]interface{}))
-      //fmt.Println(i)
-      // fmt.Println(fmt.Sprintf("time: %f, close: %f, high: %f, low: %f, open: %f, volumefrom: %f, volumeto %f", //you can just do multiline code between ( )
-      //   item["time"], item["close"], item["high"], item["low"], item["open"], item["volumefrom"], item["volumeto"])) //pull out values
       row := []string{strconv.FormatFloat(item["time"].(float64), 'E', -1, 64), strconv.FormatFloat(item["close"].(float64), 'E', -1, 64),
         strconv.FormatFloat(item["high"].(float64), 'E', -1, 64), strconv.FormatFloat(item["low"].(float64), 'E', -1, 64),
         strconv.FormatFloat(item["open"].(float64), 'E', -1, 64), strconv.FormatFloat(item["volumefrom"].(float64), 'E', -1, 64),
@@ -69,5 +65,5 @@ func main() { //https://min-api.cryptocompare.com/data/pricehistorical
     time.Sleep(1 * time.Second)
   }
   defer file.Close()
-  writer.Flush()
+  defer writer.Flush()
 }
