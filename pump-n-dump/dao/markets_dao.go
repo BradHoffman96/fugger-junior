d4cc53c8ad5c8e052b3b331f07ad214f79b7919d
Brad Hoffman <bradleydavidhoffman@gmail.com>
2018-04-23 20:23:02 -0400

    started work on the dao. need my models commited though

---
+++ pump-n-dump/dao/markets_dao.go
@@ -0,0 +1,33 @@
+package dao
+
+import (
+	"log"
+
+	"github.com/globalsign/mgo"
+)
+
+type MarketsDAO struct {
+	Server   string
+	Database string
+}
+
+var db *mgo.Database
+
+//this is going to need to be changed because there will be a collection for each market
+const (
+	COLLECTION = "markets"
+)
+
+func (m *MarketsDAO) Connect() {
+	session, err := mgo.Dial(m.Server)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		log.Println("Connected to server")
+	}
+	db = session.DB(m.Database)
+}
+
+func (m *MarketsDAO) Insert(summary Summary) {
+
+}
