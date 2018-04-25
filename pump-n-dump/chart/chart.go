e633a637edfdb8f8985a4f988bd0b63ca7f7d472
Brad Hoffman <bradleydavidhoffman@gmail.com>
2018-04-25 18:58:08 -0400

    after painstakingly trying to parse a time string in go, getValues() is now return date and price values for each summary

--- pump-n-dump/chart/chart.go
+++ pump-n-dump/chart/chart.go
@@ -4,9 +4,37 @@ import (
 	"fmt"
 	. "fugger-junior/pump-n-dump/dao"
 	"log"
+	"strings"
+	"time"
 )
 
 var dao = MarketsDAO{}
+
+func getValues() ([]time.Time, []float64) {
+	var dates []time.Time
+	var prices []float64
+
+	summaries, err := dao.FindAll("BTC-TRX")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, summary := range summaries {
+		fmt.Println(summary.TimeStamp)
+		timeString := strings.Split(summary.TimeStamp, "T0")
+		newTimeString := timeString[0] + "T" + timeString[1]
+
+		parsed, err := time.Parse("2006-01-02T15:04:05", newTimeString)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		dates = append(dates, parsed)
+		prices = append(prices, summary.Last)
+	}
+
+	return dates, prices
+}
 
 func init() {
 	dao.Server = "localhost"
@@ -15,12 +43,8 @@ func init() {
 }
 
 func main() {
-	summaries, err := dao.FindAll("BTC-TRX")
-	if err != nil {
-		log.Fatal(err)
-	}
+	dates, prices := getValues()
 
-	for _, summary := range summaries {
-		fmt.Println(summary)
-	}
+	fmt.Println(dates)
+	fmt.Println(prices)
 }
